dd46f25c349ad9c6c4777e1f75838739592337fb
gavinbrown025 <you@example.com>
2020-11-26 12:18:45 -0500

    life cycle hooks

--- js/main.js
+++ js/main.js
@@ -6,13 +6,28 @@ const myVM = (() => {
         // el:"#app",
         data: {
             message: "Hello from Vue!",
-            anotherThing: "seperate by comma",
+            anotherThing: "See My Profs",
+            removeAFormat: true,
+            showBioData: false,
+            currentProf: {},
 
-            collection: [
-                {name: "Justin", role: 'coordinator', nickname: 'bruiser'},
-                { name: "John", role: 'prof', nickname: 'Morpheus' },
-                { name: "Joe", role: 'prof', nickname: 'Kooky' }
+            professors: [
+                { name: "Justin", role: 'coordinator', nickname: 'bruiser', bio:'these are different woasdfrds'},
+                { name: "John", role: 'prof', nickname: 'Morpheus', bio: 'these are dadsfasdferent words'},
+                { name: "Joe", role: 'prof', nickname: 'Kermit', bio: 'theasdfasse are differeasdfnt words' }
             ]
+        },
+        //* THIS IS A 'MOUNTED' lifecycle hook . vue is done creating itself and has attached itself to the 'app' div on the page
+        mounted: function(){
+            console.log("VUE is mounted");
+            
+            this.professors.push({
+                name: 'Jarrod', role: 'supermodel', nickname: 'ZooLander', bio: 'theasdfasdse are different words'
+            })
+        },
+
+        updated: function(){
+            console.log('things updated');
         },
 
         methods: {
@@ -21,6 +36,11 @@ const myVM = (() => {
             },
             anotherFunction() {
                 console.log("used other function");
+            },
+            removeProf(prof){
+                console.log(prof.name);
+                this.showBioData = this.showBioData ? false : true;
+                this.currentProf = prof;
             }
         }
     }).$mount('#app');  // OPT#2 also connects Vue to your Wrapper in HTML
